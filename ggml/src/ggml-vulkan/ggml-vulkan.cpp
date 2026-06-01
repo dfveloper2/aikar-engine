169835378529a4290982f1588aa4c1ef11161a06
dfveloper_tablet <crazyimator@gmail.com>
2026-06-02 07:20:10 +0900

    I thout i commited it.

--- ggml/src/ggml-vulkan/ggml-vulkan.cpp
+++ ggml/src/ggml-vulkan/ggml-vulkan.cpp
@@ -16612,9 +16612,14 @@ static bool ggml_backend_vk_device_supports_op(ggml_backend_dev_t dev, const ggm
                     case GGML_TYPE_Q5_1:
                     case GGML_TYPE_Q8_0:
                     case GGML_TYPE_IQ4_NL:
+                    case GGML_TYPE_TURBO2_0:    // added
+                    case GGML_TYPE_TURBO3_0:    // added
+                    case GGML_TYPE_TURBO4_0:    // added
+                    case GGML_TYPE_TQ4_1S:      // added
                         return true;
                     default:
                         return false;
+                    
                 }
             }
         case GGML_OP_CONT:
